be729c37c8665268782135791502a620732ff7d0
Bopomkova <bjowens06@gmail.com>
2022-02-22 11:49:22 -0500

    Test

--- project/src/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="footer">
-    <h2>I am not legally responsible for any stupid things you do here</h2>
-  </div>
-</template>
-
-<style scoped>
-</style>
--- project/src/components/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>test home.vue</h1>
-  </div>
-</template>
-
-<style scoped>
-</style>
