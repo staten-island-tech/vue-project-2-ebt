66287bf687ecb44d9fec45869d2f5e76b4038355
TimothyL52 <timothyl52@nycstudents.net>
2022-03-10 13:13:46 -0500

    well uh function here lol look at this
    https://stackoverflow.com/questions/61728016/vue-js-change-image-src-with-a-click

--- project/components/Gallery.vue
+++ project/components/Gallery.vue
@@ -2,10 +2,9 @@
   <div class="gallery-home">
     <h1 class="gallery-title">Gallery</h1>
     <h2 class="gallery-header">Famous people ye</h2>
-    <card class="gallery-card" v-for="image in images" :key="image.name">
-      <img class="gallery-img" v-bind:src="image.url" v-bind:alt="image.alt" />
-      <p class="description">{{ image.description }}</p>
-    </card>
+    <div class="gallery-card">
+      <img class="gallery-img" src="/arcade.jpg" alt="" />
+    </div>
     <button class="gallery-button">left arrow</button>
     <button class="gallery-button">right arrow</button>
   </div>
@@ -25,7 +24,7 @@ export default {
         },
         {
           name: "this",
-          url: "lf",
+          url: "/statue.jpg",
           alt: "stuff",
           description: "Nrfhedhnmdrugebng",
         },
@@ -37,13 +36,13 @@ export default {
         },
         {
           name: "this",
-          url: "lf",
+          url: "/statue.jpg",
           alt: "stuff",
           description: "Nrfhedhnmdrugebng",
         },
         {
           name: "this",
-          url: "lf",
+          url: "/statue.jpg",
           alt: "stuff",
           description: "Nrfhedhnmdrugebng",
         },
@@ -52,6 +51,15 @@ export default {
     //need to v-bind images, look at whatever
     //v-bind and declarative rendering don't work rn for some reason, figure it out
     //make a method/function or whatever for buttons
+  },
+  mounted() {
+    this.switchImage();
+  },
+  methods: {
+    switchImage() {
+      this.image = this.images[this.index];
+      this.index = (this.index + 1) % this.images.length;
+    },
   },
 };
 </script>
@@ -67,17 +75,16 @@ export default {
   font-size: 2rem;
 }
 .gallery-card {
-  width: 30rem;
+  width: 90rem;
   background-color: #f44336;
-  height: 30rem;
-  margin: 5rem;
+  height: 70rem;
   color: #010101;
   text-align: center;
 }
-img.gallery-img {
-  height: 50%;
-  width: 60%;
+.gallery-img {
+  width: 85%;
 }
+/* find way to center card and image */
 .gallery-button {
   color: red;
 }
