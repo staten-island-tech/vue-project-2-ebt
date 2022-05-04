131120e1a97e192f51c4d5c1bcfb38938cdda357
elvenz1 <elvekins@gmail.com>
2022-05-04 12:51:23 -0400

    5/4.1

--- project/components/review-card.vue
+++ project/components/review-card.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="review-card">
-    <img class="review-img" :src="image" alt="" />
-    <h3 class="">{{ desc }}</h3>
-    <h3 class="text-size">{{ title }}</h3>
+    <img class="review-img" :src="image" alt="reviewers" />
+    <p class="text-size-review">{{ desc }}</p>
   </div>
-<!--   <div class="review-card">
-        <img class="review-img" src="reviewer5.png" alt="review1" />
-        <p class="text-size">
-          "This hotel wouldn't exist without me. This hotel is awesome because
-          of me!"
-        </p>
-      </div> -->
+
 </template>
 
 <script>
@@ -25,26 +18,6 @@ export default {
 </script>
 
 <style>
-/* .card {
-  width: 30%;
-  background-color: var(--thirdary);
-  margin: 2rem auto;
-  border-radius: 1.5rem;
-}
-
-.img-card {
-  display: block;
-  width: 100%;
-  height: 24rem;
-  object-fit: contain;
-}
-
-.title3 {
-  box-sizing: border-box;
-  font-size: 3.5rem;
-  margin: 1rem auto;
-} */
-
 .review-card {
   display: flex;
   flex-wrap: wrap;
@@ -62,7 +35,7 @@ export default {
   object-fit: cover;
 }
 
-.text-size {
+.text-size-review {
   font-size: 2.5rem;
   width: 50%;
   margin: auto;
--- project/components/reviews.vue
+++ project/components/reviews.vue
@@ -4,52 +4,18 @@
     <h3 class="section-subtitle">Why we are the best !!</h3>
     <main class="reviews">
       <h2 class="section-subtitle">5 Star Ratings ⭐⭐⭐⭐⭐</h2>
-     <!--  <div class="review-card">
-        <img class="review-img" src="reviewer5.png" alt="review1" />
-        <p class="text-size">
-          "This hotel wouldn't exist without me. This hotel is awesome because
-          of me!"
-        </p>
-      </div>
-      <div class="review-card">
-        <img class="review-img" src="reviewer3.png" alt="review1" />
-        <p class="text-size">
-          "I am planning to announce a collaboration with this hotel because
-          this hotel is so successful and will be beneficial to my business."
-        </p>
-      </div>
-      <div class="review-card">
-        <img class="review-img" src="reviewer1.png" alt="review1" />
-        <p class="text-size">
-          "I visited this hotel with my wife and we had plenty of lovely
-          memories here. I love this hotel. It is a good place to relax after a
-          busy day."
-        </p>
-      </div>
-      <div class="review-card">
-        <img class="review-img" src="reviewer4.png" alt="review1" />
-        <p class="text-size">"Safe hotel. No criminals."</p>
-      </div>
-      <div class="review-card">
-        <img class="review-img" src="reviewer2.png" alt="review1" />
-        <p class="text-size">
-          "This is my favorite place to be at after a day of hard work."
-        </p>
-      </div> -->
        <main class="margin-auto">
-      <section class="w80 flex-parent margin-auto">
+      <section class="w90 flex-parent margin-auto">
         <Review-card
           v-for="reviews in reviews"
           :key="reviews.name"
-          :title="reviews.name"
           :image="reviews.img"
           :desc="reviews.comment"
         >
         </Review-card>
       </section>
     </main>
-      <a class="text-size margin-auto w80" href="https://mikewhalen.tech/"
-        >READ MORE REVIEWS</a>
+      <a class="text-size-review margin-auto w80" href="https://mikewhalen.tech/">READ MORE REVIEWS</a>
     </main>
   </div>
 </template>
@@ -67,17 +33,17 @@ export default {
           img: "/reviewer5.png",
         },
         {
-          name: "Kentucky Chicken Guy",
+          name: "KFC Man",
           comment: "I am planning to announce a collaboration with this hotel because this hotel is so successful and will be beneficial to my business.",
           img: "/reviewer3.png",
         },
         {
-          name: "Washingmachine",
+          name: "Washton",
           comment: "I visited this hotel with my wife and we made plenty of memories here. I love this hotel. It is a good place to relax after a busy day.",
           img: "/reviewer1.png",
         },
         {
-          name: "Manbat",
+          name: "Bat",
           comment: "Safe hotel. No criminals.",
           img: "/reviewer4.png",
         },
@@ -96,29 +62,6 @@ export default {
 </script>
 
 <style>
-/* .review-card {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  width: 80%;
-  height: auto;
-  margin: 4rem auto;
-  background-color: #ffffff;
-  color: #000000;
-}
-
-.review-img {
-  width: 31rem;
-  height: 25rem;
-  object-fit: cover;
-}
-
-.text-size {
-  font-size: 2.5rem;
-  width: 50%;
-  margin: auto;
-}
- */
 .reviews {
   overflow: auto;
   overflow-x: hidden;
