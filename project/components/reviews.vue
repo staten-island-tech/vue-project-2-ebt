24a50ada538c1d9201bd118d24b6c700bbcd5123
Bopomkova <bjowens06@gmail.com>
2022-05-05 12:53:45 -0400

    Update reviews.vue

--- project/components/reviews.vue
+++ project/components/reviews.vue
@@ -4,20 +4,39 @@
     <h3 class="section-subtitle">Why we are the best !!</h3>
     <main class="reviews">
       <h2 class="section-subtitle">5 Star Ratings ⭐⭐⭐⭐⭐</h2>
-      <main class="margin-auto">
-        <section class="w90 flex-parent margin-auto">
-          <Review-card
-            v-for="reviews in reviews"
-            :key="reviews.name"
-            :image="reviews.img"
-            :desc="reviews.comment"
-          >
-          </Review-card>
-        </section>
-      </main>
-      <a
-        class="text-size-review margin-auto w80"
-        href="https://mikewhalen.tech/"
+      <div class="review-card">
+        <img class="img3" src="reviewer5.jpg" alt="review1" />
+        <p class="text-size">
+          "This hotel wouldn't exist without me. This hotel is awesome because
+          of me!"
+        </p>
+      </div>
+      <div class="review-card">
+        <img class="img3" src="reviewer3.jpg" alt="review1" />
+        <p class="text-size">
+          "I am planning to announce a collaboration with this hotel because
+          this hotel is so successful and will be beneficial to my business."
+        </p>
+      </div>
+      <div class="review-card">
+        <img class="img3" src="reviewer1.jpg" alt="review1" />
+        <p class="text-size">
+          "I visited this hotel with my wife and we had plenty of lovely
+          memories here. I love this hotel. It is a good place to relax after a
+          busy day."
+        </p>
+      </div>
+      <div class="review-card">
+        <img class="review-img" src="reviewer4.jpg" alt="review1" />
+        <p class="text-size">"Safe hotel. No criminals."</p>
+      </div>
+      <div class="review-card">
+        <img class="review-img" src="reviewer2.jpg" alt="review1" />
+        <p class="text-size">
+          "This is my favorite place to be at after a day of hard work."
+        </p>
+      </div>
+      <a class="text-size margin-auto w80" href="https://mikewhalen.tech/"
         >READ MORE REVIEWS</a
       >
     </main>
