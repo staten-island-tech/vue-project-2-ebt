95997987b6ffa3f6da17db98fa2924b8fa4f19b9
elvenz1 <elvekins@gmail.com>
2022-05-13 13:11:30 -0400

    5/13

--- project/components/Card.vue
+++ project/components/Card.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="card">
+<!--  <div class="card">
 
     <img class="img-card margin-auto w90" :src="image" alt="" />
 
+  </div> -->
+  <div class="card">
+  <img :src="image" class="img-card" alt="">
+  <div class="card-info">
+    <div class="text">{{title}}</div>
+    <div class="text">{{desc}}</div>
   </div>
+</div>
 </template>
 
 <script>
@@ -19,6 +26,58 @@ export default {
 
 <style>
 .card {
+  position: relative;
+  width: 32%;
+  margin: 2rem auto;
+  border-radius: 1.5rem;
+
+  border: solid 1rem var(--thirdary);
+}
+
+.img-card {
+  opacity: 1;
+  display: block;
+  width: 100%;
+  height: auto;
+  transition: .5s ease;
+  backface-visibility: hidden;
+  align-content:center;
+    border-radius: 1.5rem;
+}
+
+.card-info {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.card:hover .img-card {
+  opacity: 0.25;
+}
+
+.card:hover .card-info {
+  opacity: 1;
+}
+
+.text {
+  background-color: white;
+  color: black;
+  width: 22rem;
+  height: auto;
+  font-size: 2rem;
+  padding: 2rem 3rem;
+}
+
+
+
+
+
+/* .card {
   width: 30%;
   margin: 2rem auto;
   border-radius: 1.5rem;
@@ -43,9 +102,6 @@ export default {
   box-sizing: border-box;
   font-size: 3.5rem;
   margin: 1rem auto;
-}
-.desc {
-  font-size: 1.5rem;
-  margin: 1rem auto;
-}
+} */
+
 </style>
